6b4757a926655120cca79a2b9ecf0ee3b1eb0b06
ZartDrag <kartikmay25@gmail.com>
2023-06-25 18:59:03 +0530

    Added more features

---
+++ src/Components/TextArea.js
@@ -0,0 +1,155 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export default function TextArea(props) {
+  const [text, setText] = useState("Enter Text Here");
+  const [wordCount, setWordCount] = useState(text.split(" ").length);
+
+  const changeHandler = (event) => {
+    setText(event.target.value);
+    let words = [];
+    text.split(" ").forEach((word) => {
+      if (word !== "") words.push(word);
+    });
+    setWordCount(words.length);
+  };
+
+  const upClickHander = () => {
+    setText(text.toUpperCase());
+  };
+
+  const lowClickHander = () => {
+    setText(text.toLowerCase());
+  };
+
+  const capitalizedClickHander = () => {
+    let newText = "";
+    text.split(" ").forEach((word) => {
+      if (word.length !== 0)
+        newText +=
+          word[0].toUpperCase() + word.substring(1).toLowerCase() + " ";
+    });
+    console.log(newText);
+    setText(newText);
+  };
+
+  const clearClickHander = () => {
+    setText("");
+  };
+
+  const loremClickHandler = () => {
+    setText(
+      text +
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos unde mollitia repudiandae ullam soluta ad perspiciatis adipisci placeat veritatis necessitatibus sunt quas, porro eius reiciendis animi quae eligendi? Ratione eos, hic iusto enim debitis natus magni ipsam laudantium vel nam tenetur repellat rerum tempora similique. "
+    );
+  };
+
+  const sentenceClickHandler = () => {
+    let newText = "";
+    let symbol = "";
+
+    for (let i = text.length - 1; i >= 0; i--) {
+      if (text[i] === "." || text[i] === "!" || text[i] === "?") {
+        symbol = text[i];
+        break;
+      }
+    }
+
+    text
+      .toLowerCase()
+      .split(". ")
+      .forEach((sentence) => {
+        let blank = true;
+        for (let i = 0; i < sentence.length; i++) {
+          if (sentence[i] !== " ") {
+            blank = false;
+            break;
+          }
+        }
+
+        if (!blank)
+          newText += " " + sentence[0].toUpperCase() + sentence.substring(1) + ".";
+      });
+
+    let newerText = "";
+
+    newText.split("? ").forEach((sentence) => {
+      let blank = true;
+
+      for (let i = 0; i < sentence.length; i++) {
+        if (sentence[i] !== " ") {
+          blank = false;
+          break;
+        }
+      }
+      if (!blank)
+        newerText += " " + sentence[0].toUpperCase() + sentence.substring(1) + "?";
+    });
+
+    newerText = newerText.substring(0, newerText.length - 2) + symbol;
+
+    setText(newerText.trim());
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <h1>{props.heading}</h1>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="8"
+            value={text}
+            onChange={changeHandler}
+          ></textarea>
+        </div>
+
+        <button className="btn btn-primary m-3" onClick={upClickHander}>
+          Upper Case
+        </button>
+
+        <button className="btn btn-primary m-3" onClick={lowClickHander}>
+          Lower Case
+        </button>
+
+        <button
+          className="btn btn-primary m-3"
+          onClick={capitalizedClickHander}
+        >
+          Capitalized Case
+        </button>
+
+        <button className="btn btn-primary m-3" onClick={sentenceClickHandler}>
+          Sentence Case
+        </button>
+
+        <button className="btn btn-primary m-3" onClick={loremClickHandler}>
+          Sample Text
+        </button>
+
+        <button className="btn btn-primary m-3" onClick={clearClickHander}>
+          Clear
+        </button>
+      </div>
+
+      <div className="container my-3">
+        <h2>Your text Summary</h2>
+        <p>
+          Your text has {wordCount} {wordCount === 1 ? "word" : "words"} and{" "}
+          {text.length} characters.
+        </p>
+        <p>Would take {0.008 * wordCount} minutes to read. </p>
+        <h3>Text Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
+
+TextArea.propTypes = {
+  heading: PropTypes.string,
+};
+
+TextArea.defaultProps = {
+  heading: "Default Heading",
+};
